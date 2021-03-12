85c323a1564d49ece12b5b8e7ee339d4cef40a25
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-12 15:37:26 +0530

    Update IntegerMaxTest.java

--- src/test/java/com/knoldus/IntegerMaxTest.java
+++ src/test/java/com/knoldus/IntegerMaxTest.java
@@ -2,6 +2,7 @@ package com.knoldus;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 public class IntegerMaxTest {
@@ -9,7 +10,7 @@ public class IntegerMaxTest {
     @Test
     public void isMaxNumber() {
         IntegerMax obj = new IntegerMax();
-        int val = obj.CalculatingMax(6, 5);
+        int val = obj.CalculatingMax("6", "5");
         Assert.assertEquals(6, val);
 
     }
@@ -17,10 +18,17 @@ public class IntegerMaxTest {
     @Test
     public void checkForEqual() {
         IntegerMax obj = new IntegerMax();
-        int val = obj.CalculatingMax(6, 6);
+        int val = obj.CalculatingMax("6", "6");
         Assert.assertEquals(6, val);
 
     }
 
+    @Test
+    public void checkForException() {
+        IntegerMax obj = new IntegerMax();
+        Assertions.assertThrows(NumberFormatException.class,()-> obj.CalculatingMax("6", "six"));
+
+    }
 
 }
+
