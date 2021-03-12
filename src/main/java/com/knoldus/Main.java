0607ac4a61f8f77757218325a28dfd093e7c164c
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2021-03-12 15:32:56 +0530

    Create Main.java
    LambdaFirst Assignment Submitted By Gaurav Srivastav

--- src/main/java/com/knoldus/Main.java
+++ src/main/java/com/knoldus/Main.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the FirstInterger number=");
-        int FirstInteger = sc.nextInt();
-        System.out.print("Enter the SecondInterger number=");
-        int SecondInteger = sc.nextInt();
+        public static void main(String[] args) {
 
-        IntegerMax object = new IntegerMax();
-        int result= object.CalculatingMax(FirstInteger,SecondInteger);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the FirstInterger number=");
+            String FirstInteger = sc.nextLine();
+            System.out.print("Enter the SecondInterger number=");
+            String SecondInteger = sc.nextLine();
 
-        System.out.println("FirstInteger=" +FirstInteger  + " "+"SecondInteger="  +SecondInteger  + " " + "Max of them is ="+ result);
+            IntegerMax object = new IntegerMax();
+            int result= object.CalculatingMax(FirstInteger,SecondInteger);
 
+            System.out.println("FirstInteger=" +FirstInteger  + " "+"SecondInteger="  +SecondInteger  + " " + "Max of them is ="+ result);
+
+        }
     }
-}
+
+
